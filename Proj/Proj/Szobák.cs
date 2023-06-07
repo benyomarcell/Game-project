350fcf3ab730db734a51b6eb32b3dc8b5a1a7bb7
pandacska <97035019+pandacska@users.noreply.github.com>
2023-06-07 11:05:43 +0200

    Update Szobák.cs

--- Proj/Proj/Szobák.cs
+++ Proj/Proj/Szobák.cs
@@ -1,24 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Proj.Szobák;
+using static Proj.Rooms;
 
 namespace Proj
 {
-    internal class Szobák
+    internal class Rooms
     {
-        public class Room : ISzobák
+        public class Room : IRooms
         {
             public string Name { get; set; }
             public string Description { get; set; }
-            public List<ITárgyak> Items { get; set; }
-            public List<ILények> Creatures { get; set; }
-            public ISzobák North { get; set; }
-            public ISzobák South { get; set; }
-            public ISzobák East { get; set; }
-            public ISzobák West { get; set; }
+            public List<IObjects> Items { get; set; }
+            public List<IMobs> Creatures { get; set; }
+            public IRooms North { get; set; }
+            public IRooms South { get; set; }
+            public IRooms East { get; set; }
+            public IRooms West { get; set; }
         }
     }
 }
