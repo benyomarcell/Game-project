f4593dfde7a03ea2fe98096e474feed9b80248fa
pandacska <97035019+pandacska@users.noreply.github.com>
2023-06-07 11:04:29 +0200

    Update and rename ILények.cs to IMobs.cs

--- Proj/Proj/IMobs.cs
+++ Proj/Proj/IMobs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Proj
 {
-    internal interface ILények
+    internal interface IMobs
     {
         string Name { get; }
         int Health { get; }
@@ -14,6 +14,6 @@ namespace Proj
         int Defense { get; }
         int Xp { get; }
 
-        void AttackPlayer(IJátékos player);
+        void AttackPlayer(IPlayer player);
     }
 }
